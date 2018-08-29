66dfa83c5a77e3e462acf4cbf817a57138c69528
Pawel <pawel.koscielny2904@gmail.com>
2018-08-29 23:09:59 +0200

    Added Education, Skill, Project Component, finished Resume Section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,14 @@ import ja from '../img/ja.png';
 
 const Contact = () => {
   return (
-    <div className="container-fluid contact__wrapper mb-5">
+    <div className="container-fluid contact__wrapper pb-5">
       <div className="row d-flex flex-wrap">
         <div className="col-12 col-md-6 w-100 contact__section mt-5">
           <h1 className="">Paweł Kościelny</h1>
-          <hr className="alert-info"/>
-          <img className="img-fluid my-4" src={ja} alt="Pawel Koscielny"/>
+          <hr />
+          <img className="img-fluid my-5 " src={ja} alt="Pawel Koscielny"/>
           <p className="contact__about mt-3 p-4 border ">
-            Hey I'm Paul, I'm 26yrs old. Currently gradute of University of Science and Technology (AGH), where I got bachelor degree, of Mining and Geology. I'm learning programing for about 1,5 year on my own. Lately finished course "Front-End Developer", in "Centrum Szkoleń Informatycznych Vedius" in Cracow. For now I'm discovering React/Redux library, during my current projects. <br/>
+            Hey I'm Paul, I'm 26yrs old. Currently gradute of University of Science and Technology (AGH), where I got bachelor degree, of Mining and Geology. I'm learning programing for about 1,5 year on my own. Lately finished course "Front-End Developer", in "Centrum Szkoleń Informatycznych Vedius" in Cracow. <br/>
             Presently I'm' looking for my first proffesional experience where I can develop under supervision of expert developers.
           </p>
         </div>
@@ -18,11 +18,13 @@ const Contact = () => {
         <div className="col-12 col-md-6 w-100 contact__section mt-5">
           <div className="contact__secondSection">
             <h1 className="">Contact me</h1>
-            <hr className="alert-info "/>
-            <ul className="list-group mx-auto pt-5 contact__list">
+            <hr/>
+
+            <ul className="list-group mx-auto py-5 contact__list">
               <li className="list-group-item"><i className="fas fa-phone mr-3"></i><a href="tel:+48514719812">+48 514 719 812</a></li>
-              <li className="list-group-item"><i className="fas fa-envelope mr-3"></i><a href="mailto:pawelkoscielny2904@gmail.com">pawelkoscielny2904@gmail.com</a></li>
+              <li className="list-group-item"><i className="fas fa-envelope mr-3"></i><a href="mailto:pawel.koscielny2904@gmail.com">pawel.koscielny2904@gmail.com</a></li>
             </ul>
+
           </div>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import Project from './Project'
 import weatherapp from '../img/weatherapp.jpg';
 import recipeapp from '../img/recipeapp.jpg';
+
 
 const Projects = () => (
   <div className="projectWrapper">
@@ -12,60 +14,26 @@ const Projects = () => (
       </TabList>
 
       <TabPanel>
-        <div className="projectWrapper_card py-3">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="card-title">Weather App</h2>
-              <div className="projectWrapper__info">
-                <p className="card-text">My first Project with React Library. It using external weather API. With it u can easily check weather in your region! Just type name of your city and country in english</p>
-                <p>Technology Stack:</p>
-                <ul className="pl-0">
-                  <li>HTML/CSS</li>
-                  <li>Javascript</li>
-                  <li>Bootstrap</li>
-                  <li>Fetch API</li>
-                  <li>React</li>
-                </ul>
-                <img className="img-fluid "src={weatherapp} alt="Weather app"/>
-              </div>
-
-              <div className="projectWrapper__buttons d-flex justify-content-around flex-wrap">
-                <a href="">github</a>
-                <a href="">Live demo</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Project
+          projectTitle="Weather App"
+          projectDescription="My first Project with React Library. It using external weather API. With it u can easily check weather in your region! Just type name of your city and country in english"
+          techStack={['HTML/CSS', 'Bootstrap', 'Javascript', 'Fetch API', 'React']}
+          gitHub=""
+          liveDemo=""
+          imageSource={weatherapp}
+        />
       </TabPanel>
-
+      
       <TabPanel>
-        <div className="projectWrapper_card py-3">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="card-title">Recipe App</h2>
-              <div className="projectWrapper__info">
-                <p className="card-text">My second App where React is apply. It also using React-Router Module to handle subpages. I use food2fork external API to recevie data</p>
-                <p>Technology Stack:</p>
-                <ul className="pl-0">
-                  <li>HTML/CSS</li>
-                  <li>Javascript</li>
-                  <li>Bootstrap</li>
-                  <li>Fetch API</li>
-                  <li>React with Router</li>
-                </ul>
-                <img className="img-fluid "src={recipeapp} alt="Recipe app"/>
-              </div>
-
-              <div className="projectWrapper__buttons d-flex justify-content-around flex-wrap">
-                <a href="">github</a>
-                <a href="">Live demo</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Project
+          projectTitle="Recipe App"
+          projectDescription="My second App where React is apply. It also using React-Router Module to handle subpages. I use food2fork external API to recevie data"
+          techStack={['HTML/CSS', 'Bootstrap 4', 'Javascript', 'Fetch API', 'React']}
+          gitHub=""
+          liveDemo=""
+          imageSource={recipeapp}
+        />
       </TabPanel>
-
-
 
     </Tabs>
   </div>
